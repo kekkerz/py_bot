eb44fe5c99bccbcbfc21ae0ad1e103a9560efaf9
Austin Bevil <austinbevil@gt.rr.com>
2016-07-19 22:39:15 -0500

    Update config_example.py

--- src/config/config_example.py
+++ src/config/config_example.py
@@ -5,13 +5,19 @@ config = {
         'HOST':'irc.twitch.tv',
         'PORT':6667,
         'NICK':'py_bot',
-        'PASS':'', #Oauth password here.
+        'PASS':'', #Oauth goes here
         'CHAN':'#channel_name',
 
         'socket_buffer_size':1024, #Amount of bytes allowed through the socket
 
-        'debug':True, #Set to True to enable printing of additional information
+        'debug':False, #Set to True to enable printing of additional information
 
-        'log':True, #Set to True to log chat entries to file
+        'log':False, #Set to True to log chat entries to file
 	'log_dir':'',
+
+	'sub_only_links':False, #Only subs can paste links in chat
+	'link_timeout_message':'Only subs can paste links.', #Message sent to use when they are timed out
+	'link_timeout_time':30, #Duration of timeout
+
+	'default_cmd_cd':5,
 }
